3319cd4d7bccbe7f44af6a0f91a65fe1f9084dfd
Kellye Greene <zephyr-eng+kellye@gust.com>
2016-06-17 09:49:22 -0400

    Success

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project_name)
+    @backed_projects << project_name
+    project_name.backers << self
+  end
+
+  def backed_projects
+    @backed_projects
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Project
+  
+  attr_accessor :title, :backers, :add_backer
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+  def backers
+    @backers
+  end
+end
